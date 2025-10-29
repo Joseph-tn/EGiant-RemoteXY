f07a2d6410e67e86cae2ef75edade310ab627da2
Joseph-tn <blackmannfr@gmail.com>
2025-10-29 16:06:55 -0400

    New remote code with joystick and sliders commands. Logic still have issues but code compiles.

--- src/main.cpp
+++ src/main.cpp
@@ -8,46 +8,64 @@
 // RemoteXY select connection mode and include library 
 #define REMOTEXY_MODE__WIFI_POINT
 #include <WiFi.h>
+#include <cmath>
 
+#include <RemoteXY.h>
 // RemoteXY connection settings 
 #define REMOTEXY_WIFI_SSID "ESP32_AP"
 #define REMOTEXY_WIFI_PASSWORD "123456789"
 #define REMOTEXY_SERVER_PORT 6377
 #define REMOTEXY_ACCESS_PASSWORD "123456789"
 
+// you can enable debug logging to Serial at 115200
+//#define REMOTEXY__DEBUGLOG    
 
-#include <RemoteXY.h>
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__SOFTSERIAL
+
+// #include <SoftwareSerial.h>
+
+// // RemoteXY connection settings 
+// #define REMOTEXY_SERIAL_RX 2
+// #define REMOTEXY_SERIAL_TX 3
+// #define REMOTEXY_SERIAL_SPEED 9600
+
 
 // RemoteXY GUI configuration  
 #pragma pack(push, 1)  
-uint8_t RemoteXY_CONF[] =   // 79 bytes
-  { 255,4,0,0,0,72,0,19,0,0,0,69,83,80,56,50,54,54,0,0,
-  2,106,200,200,84,1,1,4,0,4,14,50,7,86,12,255,16,87,48,12,
-  26,4,11,14,9,176,109,51,89,16,176,12,26,1,255,107,57,57,40,45,
-  24,24,0,12,31,0,1,14,107,57,57,69,45,24,24,0,12,31,0 };
+uint8_t RemoteXY_CONF[] =   // 172 bytes
+  { 255,5,0,8,0,165,0,19,0,0,0,0,31,2,106,200,200,80,2,1,
+  2,3,0,5,14,20,150,150,131,12,59,59,0,2,26,31,71,238,75,75,
+  75,20,19,54,54,56,0,2,24,135,0,0,0,0,0,0,200,66,0,0,
+  160,65,0,0,32,65,0,0,0,64,24,0,131,41,3,21,35,80,1,30,
+  12,2,17,2,31,80,97,103,101,0,6,4,0,4,18,50,7,90,8,2,
+  25,75,32,2,26,71,18,60,75,75,72,20,58,58,56,0,2,24,135,0,
+  0,0,0,0,0,200,66,0,0,160,65,0,0,32,65,0,0,0,64,24,
+  0,4,10,48,7,90,133,33,74,25,160,2,26,131,41,3,21,35,88,2,
+  25,9,2,17,2,31,80,97,103,101,0,9 };
   
 // this structure defines all the variables and events of your control interface 
 struct {
 
     // input variables
-  int8_t slider_left; // from -100 to 100
-  int8_t slider_right; // from -100 to 100
+  uint8_t slider; // =1 if page is visible, else =0, from 0 to 1
+  int8_t joystick_01_x; // from -100 to 100
+  int8_t joystick_01_y; // from -100 to 100
+  int8_t slider_01; // from -100 to 100
+  int8_t slider_02; // from -100 to 100
 
-   // The Buttons are Extra, (Hint, You can use them for doing MicroAdjustmets....maybe!)
-   //Never Used!
-  uint8_t pushSwitch_01; // =1 if state is ON, else =0
-  uint8_t pushSwitch_02; // =1 if state is ON, else =0
-
+    // output variables
+  float instrument_01; // from 0 to 100
+  float instrument_02; // from 0 to 100
     // other variable
   uint8_t connect_flag;  // =1 if wire connected, else =0
 
 } RemoteXY;   
 #pragma pack(pop)
- 
-/////////////////////////////////////////////
+
+//////////////////////////////////////////////
 //           END RemoteXY include          //
 /////////////////////////////////////////////
-
 
 #define Motor_1_EN  14  
 #define Motor_1_in1 27  
@@ -66,21 +84,55 @@ void forward_motor2(int speed);
 void backward_motor2(int speed);
 void stopMotors();
 
+/* defined two arrays with a list of pins for each motor */
+unsigned char RightMotor[3] = 
+  {Motor_1_in1, Motor_1_in2, Motor_1_EN};
+unsigned char LeftMotor[3] = 
+  {Motor_2_in2 ,Motor_2_in1, Motor_2_EN};
+
+/*
+   speed control of the motor
+   motor - pointer to an array of pins
+   v - motor speed can be set from -100 to 100
+*/
+void Wheel (unsigned char * motor, int v)
+{
+  RemoteXY.instrument_01 = static_cast<float> (v);
+  if (v>100) v=100;
+  if (v<-100) v=-100;
+  if (v>0) {
+    digitalWrite(motor[0], HIGH);
+    digitalWrite(motor[1], LOW);
+    analogWrite(motor[2], v*2.55);
+  }
+  else if (v<0) {
+    digitalWrite(motor[0], LOW);
+    digitalWrite(motor[1], HIGH);
+    analogWrite(motor[2], (-v)*2.55);
+  }
+  else {
+    digitalWrite(motor[0], LOW);
+    digitalWrite(motor[1], LOW);
+    analogWrite(motor[2], 0);
+  }
+}
+
 void setup() {
-  RemoteXY_Init (); 
+  RemoteXY_Init(); 
 
   init_pinout(); 
   Serial.begin(9600);
 }
 
 void loop() { 
+   /* event handler module RemoteXY */
   RemoteXY_Handler ();
 
-  Handler(RemoteXY.slider_right, RemoteXY.slider_left);
-  
-  // Alternatively you can put your code here instead of Handler function, but Handler keeps it clean
-  
-  // do not call delay(), use instead RemoteXY_delay() 
+  Handler(RemoteXY.slider_01, RemoteXY.slider_02);
+  /* manage the right motor */
+  Wheel (RightMotor, RemoteXY.joystick_01_y - RemoteXY.joystick_01_x);
+  /* manage the left motor */
+  Wheel (LeftMotor, RemoteXY.joystick_01_y + RemoteXY.joystick_01_x);
 }
 
 void init_pinout() {
@@ -93,18 +145,18 @@ void init_pinout() {
   pinMode(Motor_2_EN,  OUTPUT);
   pinMode(Motor_2_in1, OUTPUT);
   pinMode(Motor_2_in2, OUTPUT);
-
-
 }
 
 
-/////////////////////////////////////////////
-//             The Driver                  //
-/////////////////////////////////////////////
+// /////////////////////////////////////////////
+// //             The Driver For Sliders            //
+// /////////////////////////////////////////////
 
 void Handler(int val2, int val1) {
   int speed1 = map(abs(val1), 0, 100, 0, 255); // FWD SPEED, Map val1 to PWM range (0-255)
   int speed2 = map(abs(val2), 0, 100, 0, 255); // Turn SPEED, Map val2 to PWM range (0-255)
+  float final_speed = sqrt(speed1 * speed1 + (speed2 * speed2)) / 3.6;
+  RemoteXY.instrument_01 = final_speed;
 
   // Priority to val2 for turning...why? (I want to make sure if i tell to turn it turns)
   // going forward is normal but turning is urgent.
@@ -141,7 +193,7 @@ void Handler(int val2, int val1) {
 
 
 /////////////////////////////////////////////
-//        Motor controller Funtions        //
+//        Motor controller Funtions For Sliders  //
 /////////////////////////////////////////////
 void forward_motor1(int speed) {
   analogWrite(Motor_1_EN, speed);    // Set PWM speed for motor 1
